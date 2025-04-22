4b90742d34190180990bb45dbf0f114a6a0c1b08
kotteletfisk <ostekugle@gmail.com>
2025-04-22 15:18:24 +0200

    update setup.sh

--- docker/redis-cluster/setup.sh
+++ docker/redis-cluster/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Remove previous volumes
 docker volume rm -f $(docker volume ls --filter "name=redis-cluster") &&
 
+# Start docker compose
 docker-compose up -d
 sleep 4
 
+# set up cluster of live nodes
 docker exec -it redis-cluster-redis-1-1 redis-cli --cluster create 172.38.0.11:6379 172.38.0.12:6379 172.38.0.13:6379 172.38.0.14:6379 172.38.0.15:6379 172.38.0.16:6379 --cluster-replicas 1 &&
 
 echo "finished. connection IP is 127.0.0.1:6371. Connect with 'redis-cli -c -p 6371'. test with 'CLUSTER NODES'"
